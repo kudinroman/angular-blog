246948c0c1c83ad9fc240c414f1dfe76fe4d09de
kudinroman <kudinroman82@gmail.com>
2021-05-10 14:47:34 +0300

    add PWA

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,7 +11,9 @@ import { PostComponent } from "./shared/components/post/post.component";
 import { SharedModule } from "./shared/shared.module";
 import { AuthInterceptor } from "./shared/auth.interceptor";
 import { registerLocaleData } from "@angular/common";
-import ruLocale from '@angular/common/locales/ru'
+import ruLocale from '@angular/common/locales/ru';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment'
 
 registerLocaleData(ruLocale, 'ru')
 
@@ -29,7 +31,7 @@ const INTERCEPTOR_PROVIDER: Provider = {
     PostPageComponent,
     PostComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule, SharedModule],
+  imports: [BrowserModule, AppRoutingModule, SharedModule, ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })],
   providers: [INTERCEPTOR_PROVIDER],
   bootstrap: [AppComponent],
 })
